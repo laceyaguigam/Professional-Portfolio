9417331e532560582c71679e2b149ccd3fe8491e
lacey aguigam <laceyaguigam@yahoo.com>
2023-01-29 22:01:06 -0800

    added buttons and function to direct to linkedin and github to

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// takes you to linkedin and github
+function myLinkedin () {
+    location.replace("https://www.linkedin.com/in/laceyaguigam/")
+}
+
+function myGithub () {
+    location.replace("https://github.com/laceyaguigam")
+}
+
+
+
